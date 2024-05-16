9c61cc00d6a4573ec7980aa87695d1a1fda0067d
natanferreirah <natanaelferreiradon31@gmail.com>
2024-05-15 21:22:42 -0300

    Arquivo Python

---
+++ estudo.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite n1:"))
+n2 = int(input("Digite n2:"))
+m = (n1 + n2)/2
+print(f'Sua média é {m}')
+if >=6
+    print("Aprovado")
+else:
+    print("Reprovado")
+print(f'Sua média é [m]')     
